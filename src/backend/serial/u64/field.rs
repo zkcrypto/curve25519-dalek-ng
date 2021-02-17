fcef1fa11b3d3e89a1abf8986386ba9ae375392c
Greg Fitzgerald <greg@solana.com>
2021-02-17 09:01:10 -0700

    Remove borsh instances from FieldElement51

--- src/backend/serial/u64/field.rs
+++ src/backend/serial/u64/field.rs
@@ -38,7 +38,6 @@ use zeroize::Zeroize;
 /// The backend-specific type `FieldElement51` should not be used
 /// outside of the `curve25519_dalek::field` module.
 #[derive(Copy, Clone)]
-#[cfg_attr(feature = "borsh", derive(BorshSerialize, BorshDeserialize))]
 pub struct FieldElement51(pub (crate) [u64; 5]);
 
 impl Debug for FieldElement51 {
